ff6a9c30b5ee6514910db71d5bec50ba417c30f2
Drew Conner <drew@blinkingboxes.net>
2023-02-16 17:12:44 -0600

    initial commit, adding first few dotfiles and super basic install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -bs $PWD/configs/tmux.conf ~/.tmux.conf
+ln -bs $PWD/configs/vimrc ~/.vimrc
+ln -bs $PWD/configs/zshrc ~/.zshrc
+# might want to add a flag for whether or not this is a personal or
+# professional install and only install personal .gitconfig if appropriate
+ln -bs $PWD/configs/gitconfig ~/.gitconfig
