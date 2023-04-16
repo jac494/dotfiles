5bbce2d9b09f9f9a3003096c483e0a199f058832
Drew Conner <drew@blinkingboxes.net>
2023-04-16 19:28:54 +0000

    added line to create vim cache dir in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 ln -bs $PWD/configs/tmux.conf ~/.tmux.conf
+
+# create vim cache directory
+mkdir -p $HOME/.cache/vim
 ln -bs $PWD/configs/vimrc ~/.vimrc
+
 ln -bs $PWD/configs/zshrc ~/.zshrc
+
 # might want to add a flag for whether or not this is a personal or
 # professional install and only install personal .gitconfig if appropriate
 ln -bs $PWD/configs/gitconfig ~/.gitconfig
