86267becbf5d302798ed846e25c243c6a82da672
Drew Conner <drew@blinkingboxes.net>
2023-05-08 20:25:24 -0500

    added gdbinit

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ ln -bs $PWD/configs/vimrc ~/.vimrc
 
 ln -bs $PWD/configs/zshrc ~/.zshrc
 
+ln -bs $PWD/configs/gdbinit ~/.gdbinit
+
 # might want to add a flag for whether or not this is a personal or
 # professional install and only install personal .gitconfig if appropriate
 ln -bs $PWD/configs/gitconfig ~/.gitconfig
