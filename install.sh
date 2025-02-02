4da7239436919982493ce66d6b15533e16ae3098
Drew Conner <drew@blinkingboxes.net>
2025-02-02 11:13:04 -0600

    adds configs/bashrc and install symlink in install.sh; install.sh fixes for shellcheck suggestions

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
-ln -bs $PWD/configs/tmux.conf ~/.tmux.conf
+#!/bin/sh
+
+ln -bs "$PWD"/configs/tmux.conf ~/.tmux.conf
 
 # create vim cache directory
-mkdir -p $HOME/.cache/vim
-ln -bs $PWD/configs/vimrc ~/.vimrc
+mkdir -p "$HOME"/.cache/vim
+ln -bs "$PWD"/configs/vimrc ~/.vimrc
 
-ln -bs $PWD/configs/zshrc ~/.zshrc
+ln -bs "$PWD"/configs/zshrc ~/.zshrc
 
-ln -bs $PWD/configs/gdbinit ~/.gdbinit
+ln -bs "$PWD"/configs/gdbinit ~/.gdbinit
 
 # might want to add a flag for whether or not this is a personal or
 # professional install and only install personal .gitconfig if appropriate
-ln -bs $PWD/configs/gitconfig ~/.gitconfig
+ln -bs "$PWD"/configs/gitconfig ~/.gitconfig
+
+# ok maybe weird to have this shell config _and_ zsh
+# but sometimes I land on a new system with bash and
+# want this available and don't care too much about
+# setting up zsh or even having it installed, primarily homelab shit
+ln -bs "$PWD"/configs/bashrc ~/.bashrc
